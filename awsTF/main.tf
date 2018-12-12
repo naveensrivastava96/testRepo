b4d34963d20c82b4d6ace9af19c73b7517887109
naveensrivastava96 <naveensrivastava96@gmail.com>
2018-12-12 11:25:41 +0530

    aws tf

---
+++ awsTF/main.tf
@@ -0,0 +1,16 @@
+
+provider "aws" {
+   region     = "ca-central-1"
+} 
+
+resource "aws_instance" "example" {
+  ami = "ami-0bce70e886ac6890a"
+  instance_type = "t2.micro"
+tags {
+Name = "Env-Test"
+}
+}
+
+
+
+ 
