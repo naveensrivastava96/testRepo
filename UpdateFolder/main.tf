792542f1c675ca52c9cf5ed752d47b209be10fbd
naveensrivastava96 <naveensrivastava96@gmail.com>
2019-09-09 12:58:58 +0530

    Create main.tf

---
+++ UpdateFolder/main.tf
@@ -0,0 +1,17 @@
+variable "access_key" { default="asddad" }
+variable "secret_key" {default="asdfasdfas"}
+ 
+provider "aws" {
+    access_key = "${var.access_key}"
+   secret_key = "${var.secret_key}"
+   region     = "ca-central-1"
+} 
+
+resource "aws_instance" "example" {
+  ami = "ami-0bce70e886ac6890a"
+  instance_type = "t2.micro"
+tags {
+Name = "Updated-Demo-ENV"
+}
+}
+
