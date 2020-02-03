0fb111f6fd35e68623efc58ccd9c434d41627b60
naveensrivastava96 <naveensrivastava96@gmail.com>
2020-02-03 14:40:34 +0530

    Update main.tf

--- awsTF/main.tf
+++ awsTF/main.tf
@@ -17,5 +17,26 @@ Name = "Demo-ENV"
 }
 }
 
+provider "docker" {
+  host = "http://10.243.41.201:4243"
+}
+
+resource "docker_container" "ca-sample" {
+  name  = "${random_string.random_name.result}"
+  image = "${docker_image.dummy_image.latest}"
+  entrypoint = ["/bin/sleep"]
+  command = [ "1d" ]
+}
+
+resource "docker_image" "dummy_image" {
+  name = "busybox:latest"
+}
+
+resource "random_string" "random_name" {
+	length  = 5
+	special = false
+	lower   = false
+}
+
 
  
