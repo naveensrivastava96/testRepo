6be69d2fb3363d718f2480efb2675593831b3175
naveensrivastava96 <naveensrivastava96@gmail.com>
2020-02-03 15:33:35 +0530

    Update main.tf

--- awsTF/main.tf
+++ awsTF/main.tf
@@ -17,26 +17,59 @@ Name = "Demo-ENV"
 }
 }
 
-provider "docker" {
-  host = "http://10.243.41.201:4243"
+
+provider "cda" {
+	cda_server = "${var.cda_server}"
+	user       = "${var.cda_user}"
+	password   = "${var.cda_password}"
+  default_attributes = {
+		folder = "DEMOAPP"
+		owner = "200/AUTOMIC/AUTOMIC"		
+  }
 }
 
-resource "docker_container" "ca-sample" {
-  name  = "${random_string.random_name.result}"
-  image = "${docker_image.dummy_image.latest}"
-  entrypoint = ["/bin/sleep"]
-  command = [ "1d" ]
+resource "random_integer" "name_extension" {
+  min     = 1
+  max     = 99999
 }
 
-resource "docker_image" "dummy_image" {
-  name = "busybox:latest"
+#resource "cda_deployment_profile" "myProfile" {
+#  name  = "my_profile_${random_integer.name_extension.result}"
+#  application  = "DemoApp"
+#  login_object = "Login.All"
+#  environment  = "Local"
+#  description = "My Profile"
+#   deployment_map = {
+#   "database" = "${cda_deployment_target.myTarget1.name}",
+#   "webapp" = "${cda_deployment_target.myTarget2.name}"
+#  } 
+#}
+
+#resource "cda_workflow_execution" "my_execution" {
+#  triggers = "true" 
+#  application                  = "DemoApp" 
+#  workflow                     = "Deploy" 
+#  package                      = "1" 
+#  deployment_profile           = "my_profile_${random_integer.name_extension.result}" 
+#  manual_approval              = "true" 
+#  approver                     = "${var.user}"
+#  schedule                     = "2019-12-28T13:44:00Z" //"cron(0 3 12 12 ? 2019)" //start_date = "2019-12-12" //start_time = "3:00"  
+#  override_existing_components = "true"
+#  depends_on = [
+#  	cda_deployment_profile.myProfile    
+#    ]  
+#}
+
+variable "cda_server" {
+  default = "http://10.108.54.205:8080/awi"
 }
 
-resource "random_string" "random_name" {
-	length  = 5
-	special = false
-	lower   = false
+variable "cda_user" {
+  default = "400/AUTOMIC/AUTOMIC"
 }
 
+variable "cda_password" {
+	default = "test"
+}
 
  
