65e9c875277bd33ec86ebc0e2f684be395828066
naveensrivastava96 <naveensrivastava96@gmail.com>
2020-02-05 12:33:16 +0530

    Update main.tf

--- awsTF/main.tf
+++ awsTF/main.tf
@@ -17,58 +17,16 @@ Name = "Demo-ENV"
 }
 }
 
-provider "cda" {
-	cda_server = "${var.cda_server}"
-	user       = "${var.cda_user}"
-	password   = "${var.cda_password}"
-  default_attributes = {
-		folder = "DEMOAPP"
-		owner = "200/AUTOMIC/AUTOMIC"		
-  }
+variable "github_token" {}
+
+provider "github" {
+    #create account and get token
+    token = "${var.github_token}"
 }
 
-resource "random_integer" "name_extension" {
-  min     = 1
-  max     = 99999
+resource "github_repository" "sample-terraform-app" {
+    name = "sample-terraform-app"
+    description = "Sample terraform app demo"
+    
+    auto_init = true # initial commit provision
 }
-
-#resource "cda_deployment_profile" "myProfile" {
-#  name  = "my_profile_${random_integer.name_extension.result}"
-#  application  = "DemoApp"
-#  login_object = "Login.All"
-#  environment  = "Local"
-#  description = "My Profile"
-#   deployment_map = {
-#   "database" = "${cda_deployment_target.myTarget1.name}",
-#   "webapp" = "${cda_deployment_target.myTarget2.name}"
-#  } 
-#}
-
-#resource "cda_workflow_execution" "my_execution" {
-#  triggers = "true" 
-#  application                  = "DemoApp" 
-#  workflow                     = "Deploy" 
-#  package                      = "1" 
-#  deployment_profile           = "my_profile_${random_integer.name_extension.result}" 
-#  manual_approval              = "true" 
-#  approver                     = "${var.user}"
-#  schedule                     = "2019-12-28T13:44:00Z" //"cron(0 3 12 12 ? 2019)" //start_date = "2019-12-12" //start_time = "3:00"  
-#  override_existing_components = "true"
-#  depends_on = [
-#  	cda_deployment_profile.myProfile    
-#    ]  
-#}
-
-variable "cda_server" {
-  default = "http://10.108.54.205:8080/awi"
-}
-
-variable "cda_user" {
-  default = "400/AUTOMIC/AUTOMIC"
-}
-
-variable "cda_password" {
-	default = "test"
-}
-
- 
