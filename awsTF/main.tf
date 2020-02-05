e00d2c101053376be88726ca858f1ed5f46ccc73
naveensrivastava96 <naveensrivastava96@gmail.com>
2020-02-05 12:56:59 +0530

    Update main.tf

--- awsTF/main.tf
+++ awsTF/main.tf
@@ -22,11 +22,11 @@ variable "github_token" {}
 provider "github" {
     #create account and get token
     token = "${var.github_token}"
+  organization = "testing-for-terraform"
 }
 
 resource "github_repository" "sample-terraform-app" {
-    name = "sample-terraform-app"
+    name = "sample-terraform-app-1"
     description = "Sample terraform app demo"
-    
     auto_init = true # initial commit provision
 }
