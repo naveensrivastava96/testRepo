2f253570191c7f0431a9494958f7aae07dcbb14b
naveensrivastava96 <naveensrivastava96@gmail.com>
2020-03-27 11:58:07 +0530

    Update main.tf

--- awsTF/main.tf
+++ awsTF/main.tf
@@ -1,20 +1,15 @@
 
-variable "aws_access_key" { default="asddad" }
-variable "aws_secret_key" {default="asdfasdfas"}
-
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-   secret_key = "${var.aws_secret_key}"
-   region     = "ca-central-1"
-} 
-
-resource "aws_instance" "example" {
-  ami = "ami-0bce70e886ac6890a"
-  instance_type = "t2.micro"
-tags {
-Name = "Demo-ENV"
+variable "heroku_token" {}
+provider "heroku" {
+  email   = "vatsalgarg17@gmail.com"
+  api_key = "${var.heroku_token}"
 }
+
+resource "heroku_app" "example" {
+  name = "caratesting"
+  region = "us"
 }
+
 variable "github_token" {}
 
 provider "github" {
